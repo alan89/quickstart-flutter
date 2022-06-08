ee21e9211f5df37f89f7542642e8ba302750b2a2
gonzalezge <gerardo.cardosa@gmail.com>
2022-06-07 21:54:50 -0700

    feat: Added a Google signin option

--- authentication/lib/src/auth_providers_table.dart
+++ authentication/lib/src/auth_providers_table.dart
@@ -1,6 +1,7 @@
+import 'package:authentication/src/google_auth.dart';
+import 'package:authentication/src/home_page.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
-import 'login_page.dart';
 
 class AuthProvidersTable extends StatefulWidget {
   const AuthProvidersTable({Key? key}) : super(key: key);
@@ -10,7 +11,6 @@ class AuthProvidersTable extends StatefulWidget {
 }
 
 class _AuthProvidersTableState extends State<AuthProvidersTable> {
-
   @override
   void initState() {
     super.initState();
@@ -18,21 +18,22 @@ class _AuthProvidersTableState extends State<AuthProvidersTable> {
 
   @override
   Widget build(BuildContext context) {
-    return  ListView(
+    return ListView(
       padding: const EdgeInsets.all(8),
-      children: <Widget> [
+      children: <Widget>[
         ListTile(
           title: Text('Email/Password'),
           leading: Icon(Icons.person),
           onTap: () {
-              log("Nav to a new window");
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
+            log("Nav to a new window");
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
         ),
-        ListTile( title: Text('Passwordless'), leading: Icon(Icons.key))
+        const ListTile(title: Text('Passwordless'), leading: Icon(Icons.key)),
+        const GoogleAuthTile()
       ],
     );
   }
---
+++ authentication/lib/src/google_auth.dart
@@ -0,0 +1,43 @@
+import 'package:authentication/src/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class GoogleAuthTile extends StatelessWidget {
+  const GoogleAuthTile({Key? key}) : super(key: key);
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  void signIn(BuildContext context) async {
+    final userCredential = await signInWithGoogle();
+    print(userCredential.user);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const HomePage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('Google'),
+      leading: const Icon(Icons.android),
+      onTap: () => signIn(context),
+    );
+  }
+}

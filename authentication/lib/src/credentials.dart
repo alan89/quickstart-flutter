a052e3f9b576ecfd1312fd782280655755f4b772
gonzalezge <gerardo.cardosa@gmail.com>
2022-06-09 16:28:40 -0700

    feat: Added Twitter Auth tile

---
+++ authentication/lib/src/credentials.dart
@@ -0,0 +1,6 @@
+class CredentialsStrings {
+  // Twitter information
+  static String twitterApiKey = '<your consumer key>';
+  static String twitterApiSecretKey = ' <your consumer secret>';
+  static String twitterRedirectURI = '<your_scheme>:';
+}

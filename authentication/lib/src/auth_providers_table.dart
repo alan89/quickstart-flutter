7641f5a417ae27d458860079e7c4be1b53b6f229
“Alan <alan.escobar.martinez@gmail.com>
2022-06-10 11:28:27 -0500

    quick fixes for providers table functionality

--- authentication/lib/src/auth_providers_table.dart
+++ authentication/lib/src/auth_providers_table.dart
@@ -9,7 +9,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:authentication/src/anonymous.dart';
 import 'package:authentication/src/phone_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 class AuthProvidersTable extends StatefulWidget {
   const AuthProvidersTable({Key? key}) : super(key: key);
@@ -69,11 +68,8 @@ class _AuthProvidersTableState extends State<AuthProvidersTable> {
         const TwitterAuthTile(),
         const AnonymousAuthTile(),
         const GithubAuthTile(),
-<<<<<<< HEAD
         const PhoneAuthTile(),
-=======
         const CustomAuthTile()
->>>>>>> 81493a3bbca7605b327447a3471105b787b43c34
       ],
     );
   }
